03a916adb746d34c709ed7365d0f31f95fd5bd32
Tanish Shinde <cypher.enigma@aol.com>
2020-09-20 18:27:31 +0530

    🚀   Let's Begin

---
+++ mod.ts
@@ -0,0 +1,82 @@
+/**
+ * @param arr 
+ * @param key
+ */
+export const extractObjectValue = <T>(arr: T[], key: keyof T) => arr.map(item => item[key])
+
+/**
+ * @param array 
+ * @param target 
+ * @param keyword 
+ */
+export const narrowdownArrayObject = <T>(array: T[], target: keyof T, keyword: T[keyof T]) => 
+  array.filter(object => object[target] === keyword)
+
+/**
+ * @param file 
+ */
+export const isExistFile = async (file: string) => {
+  try {
+    await Deno.stat(file)
+    return true
+  } catch (err) {
+    if (err) return false
+  }
+}
+
+/**
+ * @param file
+ */
+export const isExistFileSync = (file: string) => {
+  try {
+    Deno.statSync(file)
+    return true
+  } catch (err) {
+    if (err) return false
+  }
+}
+
+/**
+ * @param rawdata
+ * @param file
+ */
+export const writeFile = async (rawdata: string, file: string) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(rawdata)
+  await Deno.writeFile(file, data)
+}
+
+/**
+ * @param rawdata 
+ * @param file
+ */
+export const writeFileSync = (rawdata: string, file: string) => {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(rawdata)
+  Deno.writeFileSync(file, data)
+}
+
+/**
+ * @param file
+ */
+export const readFile = async (file: string) => {
+  const decoder = new TextDecoder('utf-8')
+  const data = await Deno.readFile(file)
+  return decoder.decode(data)
+}
+
+/**
+ *
+ * @param file 
+ */
+export const readFileSync = (file: string) => {
+  const decoder = new TextDecoder('utf-8')
+  const data = Deno.readFileSync(file)
+  return decoder.decode(data)
+}
+
+/**
+ * 
+ * @param num 
+ */
+export const zeroPadding = (num: number) => String(num).padStart(2, '0')
